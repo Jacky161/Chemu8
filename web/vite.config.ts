39c7d9415cf0f72969c569b50e5c7a762f70c16c
Jacky161 <jacky.161@outlook.com>
2026-08-27 00:03:18 -0600

    feat: favicon and cloudflare wrangler manual deploy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import wasm from "vite-plugin-wasm";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     wasm(),
+    cloudflare(),
   ],
   build: {
     target: "esnext",
